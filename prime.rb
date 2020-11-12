31eea7aad09566b601c005397d183fd2f0da7e24
william <masterpkpk@gmail.com>
2020-11-12 16:11:28 -0500

    commit tree bb0d7290f00abe1980c61b88377720f6192a7b1c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+    range = (2..num - 1).to_a
+    if num < 2 
+        false
+    elsif range.each do |number|
+        if num % number == 0
+            return false
+        end
+    end
+        true
+    end    
+end
